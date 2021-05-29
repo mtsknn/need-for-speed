29f38b4767de88e10d6f204026fa62ac344d5352
Matias Kinnunen <github@mtsknn.fi>
2021-05-29 10:28:07 +0300

    Create initial component for showing results

--- src/need_for_speed/core.cljs
+++ src/need_for_speed/core.cljs
@@ -8,17 +8,17 @@
 ;; Models
 
 (defonce cars
-  [{:name "A" :consumption 3.0}
-   {:name "B" :consumption 3.5}
-   {:name "C" :consumption 4.0}])
+  {:A {:consumption 3.0}
+   :B {:consumption 3.5}
+   :C {:consumption 4.0}})
 
 ;; -------------------------
 ;; States
 
 (defonce form
-  (r/atom {:car (:name (first cars))
-           :distance 100
-           :speed-x 60
+  (r/atom {:car (-> cars first key)
+           :distance 200
+           :speed-x 80
            :speed-y 120}))
 
 (defn set-car [new-car]
@@ -32,20 +32,20 @@
 ;; -------------------------
 ;; Views
 
-(defn car-checkbox [{:keys [name consumption]}]
+(defn car-checkbox [[key {:keys [:consumption]}]]
   [:label.block
    [:input {:type "radio"
             :name "Car"
-            :value name
-            :checked (= name (:car @form))
-            :on-change #(set-car name)}]
-   (gstring/format "Car %s (%.1f litres/100km at 1km/h)" name consumption)])
+            :value key
+            :checked (= key (@form :car))
+            :on-change #(set-car key)}]
+   (gstring/format "Car %s (%.1f litres/100 km at 1 km/h)" (name key) consumption)])
 
 (defn car-checkboxes []
   [:fieldset
    [:legend "Choose your car"]
    (for [car cars]
-     ^{:key (:name car)} [car-checkbox car])])
+     ^{:key (key car)} [car-checkbox car])])
 
 (defn number-input [label key]
   [:label.block
@@ -55,13 +55,31 @@
                                    :on-change #(set-number-input key %)}]
    label])
 
+(defn results []
+  (let [base-fuel (-> (@form :car) cars :consumption)
+        distance (@form :distance)
+        speed-x (@form :speed-x)
+        speed-x-multiplier (Math/pow 1.009 (dec speed-x))
+        speed-y (@form :speed-y)
+        speed-y-multiplier (Math/pow 1.009 (dec speed-y))
+        fuel-x-per-100 (* (/ distance 100) base-fuel speed-x-multiplier)
+        fuel-y-per-100 (* (/ distance 100) base-fuel speed-y-multiplier)
+        time-x (/ distance speed-x)
+        time-y (/ distance speed-y)]
+    [:div
+     [:div (gstring/format "Fuel at speed X: %.2f litres" fuel-x-per-100)]
+     [:div (gstring/format "Fuel at speed Y: %.2f litres" fuel-y-per-100)]
+     [:div (gstring/format "Time at speed X: %.2f hours" time-x)]
+     [:div (gstring/format "Time at speed Y: %.2f hours" time-y)]]))
+
 (defn app []
   [:div.font-mono.max-w-xl.mx-auto.p-6
    [:h1.text-2xl "Need for Speed"]
    [car-checkboxes]
-   [number-input "Distance" :distance]
-   [number-input "Speed X" :speed-x]
-   [number-input "Speed Y" :speed-y]])
+   [number-input "Distance (km)" :distance]
+   [number-input "Speed X (km/h)" :speed-x]
+   [number-input "Speed Y (km/h)" :speed-y]
+   [results]])
 
 ;; -------------------------
 ;; Initialize app
