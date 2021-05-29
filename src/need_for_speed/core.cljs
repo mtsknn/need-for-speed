5dc7a0f8c1e4c73428e4d5678f13ef5b4c6faa97
Matias Kinnunen <github@mtsknn.fi>
2021-05-29 08:35:59 +0300

    Add state handling to form elements

--- src/need_for_speed/core.cljs
+++ src/need_for_speed/core.cljs
@@ -13,35 +13,61 @@
    {:name "C" :consumption 4.0}])
 
 ;; -------------------------
+;; States
+
+(defonce form
+  (r/atom {:car (:name (first cars))
+           :distance 100
+           :speed-x 60
+           :speed-y 120}))
+
+(defn set-car [new-car]
+  (swap! form assoc :car new-car))
+
+(defn set-number-input [key event]
+  (let [value (-> event .-target .-value int)]
+    (when (< 0 value 10000) ;; TODO: Prolly replace with validation messages
+      (swap! form assoc key value))))
+
+;; -------------------------
 ;; Views
 
 (defn car-checkbox [{:keys [name consumption]}]
   [:label.block
-   [:input {:type "radio" :name "car"}]
+   [:input {:type "radio"
+            :name "Car"
+            :value name
+            :checked (= name (:car @form))
+            :on-change #(set-car name)}]
    (gstring/format "Car %s (%.1f litres/100km at 1km/h)" name consumption)])
 
-(defn number-input [label name default-value]
+(defn car-checkboxes []
+  [:fieldset
+   [:legend "Choose your car"]
+   (for [car cars]
+     ^{:key (:name car)} [car-checkbox car])])
+
+(defn number-input [label key]
   [:label.block
-   [:input.border.border-gray-400 {:type "number" :name name :defaultValue default-value}]
+   [:input.border.border-gray-400 {:type "number"
+                                   :name label
+                                   :value (@form key)
+                                   :on-change #(set-number-input key %)}]
    label])
 
-(defn home-page []
-  [:div
+(defn app []
+  [:div.font-mono.max-w-xl.mx-auto.p-6
    [:h1.text-2xl "Need for Speed"]
-   [:form
-    [:fieldset
-     [:legend "Choose your car"]
-     (for [car cars]
-       ^{:key (:name car)} [car-checkbox car])]
-    [number-input "Distance" "distance" 100]
-    [number-input "Speed X" "speed-x" 60]
-    [number-input "Speed Y" "speed-y" 120]]])
+   [car-checkboxes]
+   [number-input "Distance" :distance]
+   [number-input "Speed X" :speed-x]
+   [number-input "Speed Y" :speed-y]])
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (d/render [home-page] (.getElementById js/document "app")))
+  (d/render [app] (.getElementById js/document "app")))
 
 (defn ^:export init! []
   (mount-root))
