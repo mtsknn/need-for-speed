580d1fa93892ab8b739f37410206d0ab8c88ce64
Matias Kinnunen <github@mtsknn.fi>
2021-05-31 07:20:57 +0300

    Move state setters to component functions
    This reduces the setters' scope,
    i.e. they are not available elsewhere

--- src/need_for_speed/form.cljs
+++ src/need_for_speed/form.cljs
@@ -4,34 +4,22 @@
    [need-for-speed.data :as data]
    [reagent.core :as r]))
 
-;; -------------------------
-;; State
-
 (defonce state
   (r/atom {:car (-> data/cars first key)
            :distance 200
            :speed-x 80
            :speed-y 120}))
 
-(defn- set-car [new-car]
-  (swap! state assoc :car new-car))
-
-(defn- set-number-input [key event]
-  (let [value (-> event .-target .-value int)]
-    (when (< 0 value 10000) ;; TODO: Prolly replace with validation messages
-      (swap! state assoc key value))))
-
-;; -------------------------
-;; Views
-
 (defn- car-checkbox [[key {:keys [:consumption]}]]
-  [:label.block
-   [:input {:type "radio"
-            :name "Car"
-            :value key
-            :checked (= key (@state :car))
-            :on-change #(set-car key)}]
-   (gstring/format "Car %s (%.1f litres/100 km at 1 km/h)" (name key) consumption)])
+  (let [set-car #(swap! state assoc :car %)]
+    (fn []
+      [:label.block
+       [:input {:type "radio"
+                :name "Car"
+                :value key
+                :checked (= key (@state :car))
+                :on-change #(set-car key)}]
+       (gstring/format "Car %s (%.1f litres/100 km at 1 km/h)" (name key) consumption)])))
 
 (defn- car-checkboxes []
   [:fieldset
@@ -40,12 +28,17 @@
      ^{:key (key car)} [car-checkbox car])])
 
 (defn- number-input [label key]
-  [:label.block
-   [:input.border.border-gray-400 {:type "number"
-                                   :name label
-                                   :value (@state key)
-                                   :on-change #(set-number-input key %)}]
-   label])
+  (let [set-input-value (fn [event]
+                          (let [value (-> event .-target .-value int)]
+                            (when (< 0 value 10000) ;; TODO: Replace with validation messages?
+                              (swap! state assoc key value))))]
+    (fn []
+      [:label.block
+       [:input.border.border-gray-400 {:type "number"
+                                       :name label
+                                       :value (@state key)
+                                       :on-change #(set-input-value %)}]
+       label])))
 
 (defn view []
   [:form
